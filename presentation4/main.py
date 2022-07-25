da25df7c05dd1c41a1c7e11081e01415807a2061
QingYun <a3302a0229@gmail.com>
2022-07-25 18:35:46 +0800

    隨便寫個fib

---
+++ presentation4/main.py
@@ -0,0 +1,16 @@
+def fib(num: int):
+    temp = [0, 1, 1]
+    assert num > -1
+    if num <= 2:
+        return temp[num]
+
+    for i in range(2, num):
+        temp.append(temp[i - 1] + temp[i])
+
+    return temp[-1]
+
+
+if __name__ == '__main__':
+    ans = fib(10)
+
+    print(ans)
